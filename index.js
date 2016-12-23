36ccc6c6dbb023ce218a42599ecd1e7df3e2097f
LeuisKen <leuisken@gmail.com>
2016-12-23 18:03:20 +0800

    修正语法至es6版本

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
+'use babel';
+
 /**
  * @author email:8427003@qq.com 百度hi：雷锋with红领巾
  * @file baidu fecs 代码检查插件;
  */
-var fecs = require('fecs');
-var Atom = require('atom');
-var path = require('path');
 
-var statusBar = null;
-var disposables = null;
-var mainDisposables = null;
-var editor = null;
-var errorLineSet = null;
+import fecs from 'fecs';
+import path from 'path';
+import {CompositeDisposable} from 'atom';
 
-var SEVERITY = {
+let statusBar = null;
+let disposables = null;
+let mainDisposables = null;
+let editor = null;
+let errorLineSet = null;
+
+const SEVERITY = {
     ERROR: 2,
     WARNING: 1
 };
 
-var decorConfig = {
+const checkExtMap = {
+    '.js': true,
+    '.html': true,
+    '.css': true,
+    '.less': true,
+    '.sass': true
+};
+
+const decorConfig = {
     lineWarning: {
         'type': 'line',
         'class': 'fecs-line-warning'
@@ -36,22 +47,56 @@ var decorConfig = {
     }
 };
 
-var checkExtMap = {
-    '.js': true,
-    '.html': true,
-    '.css': true,
-    '.less': true,
-    '.sass': true
-};
+const fecsStatusBar = document.createElement('a');
+fecsStatusBar.setAttribute('id', 'fecs-statusbar');
 
-function checkHandler(success, error) {
-    error = error || [];
+function getPath() {
+    return editor && editor.getPath();
+}
+
+function getOptions() {
+    let options = fecs.getOptions() || {};
+    options.reporter = 'baidu';
+    options._ = [];
+    options._.push(getPath());
+
+    return options;
+}
+
+function registerEvents() {
+    editor = atom.workspace.getActiveTextEditor();
+    if (!editor) {
+        return;
+    }
+
+    let filePath = getPath();
+    if (!filePath) {
+        return;
+    }
+    let ext = path.extname(filePath);
+    if (!checkExtMap[ext]) {
+        return;
+    }
+    disposables && disposables.dispose();
+    disposables = new CompositeDisposable();
+    disposables.add(editor.onDidSave(onSaveHandler));
+    disposables.add(editor.onDidChangeCursorPosition(onChangeCursorPositionHandler));
+
+    onSaveHandler();
+}
+
+function onSaveHandler() {
+    let options = getOptions();
+    fecs.check(options, checkHandler);
+}
+
+function checkHandler(success, error = []) {
     error.length === 0 && error.push({});
-    var errors = error[0].errors || [];
-    var marker = null;
+    let errors = error[0].errors || [];
+    let marker = null;
     errorLineSet = {};
     clearOldMarks();
-    for (var i = 0, item = null, size = errors.length; i < size; i++) {
+    for (let i = 0, item = null, size = errors.length; i < size; i++) {
         item = errors[i];
         marker = editor.markBufferRange([[item.line - 1, 1], [item.line - 1, 1]]);
 
@@ -68,89 +113,50 @@ function checkHandler(success, error) {
     }
     updateStatusBar();
 }
-function clearOldMarks() {
-    var decors = null;
-    var keys = Object.keys(decorConfig);
 
-    for (var i = 0; i < keys.length; i++) {
+function clearOldMarks() {
+    let decors = null;
+    let keys = Object.keys(decorConfig);
+
+    for (let i = 0; i < keys.length; i++) {
         decors = editor.getDecorations(decorConfig[keys[i]]);
-        for (var j = 0; j < decors.length; j++) {
+        for (let j = 0; j < decors.length; j++) {
             decors[j].destroy();
         }
     }
 }
 
-function onSaveHandler() {
-    var options = getOptions();
-    fecs.check(options, checkHandler);
-}
 function onChangeCursorPositionHandler() {
     updateStatusBar();
 }
-
-function getOptions() {
-    var options = fecs.getOptions() || {};
-    options.reporter = 'baidu';
-    options._ = [];
-    options._.push(getPath());
-
-    return options;
-}
-function getPath() {
-    return editor && editor.getPath();
-}
-
-var fecsStatusBar = document.createElement('a');
-fecsStatusBar.setAttribute('id', 'fecs-statusbar');
 
 function updateStatusBar() {
     if (!editor) {
         return;
     }
-    var line = editor.getCursorBufferPosition().row + 1;
-    var item = errorLineSet  && errorLineSet[line];
+    let line = editor.getCursorBufferPosition().row + 1;
+    let item = errorLineSet && errorLineSet[line];
 
-    var text = '';
+    let text = '';
     if (item) {
         text = 'fecs: ' + item.line + ':' + item.column + item.message + ' [' + item.rule + ']';
     }
     fecsStatusBar.textContent = text;
     fecsStatusBar.setAttribute('title', text);
 }
-function registerEvents() {
-    editor = atom.workspace.getActiveTextEditor();
-    if (!editor) {
-        return;
-    }
 
-    var filePath = getPath();
-    if (!filePath) {
-        return;
-    }
-    var ext = path.extname(filePath);
-
-    if (!checkExtMap[ext]) {
-        return;
-    }
-    disposables && disposables.dispose();
-    disposables = new Atom.CompositeDisposable();
-    disposables.add(editor.onDidSave(onSaveHandler));
-    disposables.add(editor.onDidChangeCursorPosition(onChangeCursorPositionHandler));
-    onSaveHandler();
-}
-module.exports = {
-    activate: function () {
-        mainDisposables = new Atom.CompositeDisposable();
+export default {
+    activate() {
+        mainDisposables = new CompositeDisposable();
         mainDisposables.add(atom.workspace.observeActivePaneItem(registerEvents));
     },
-    consumeStatusBar: function ($statusBar) {
+    consumeStatusBar($statusBar) {
         statusBar = $statusBar;
         statusBar.addLeftTile({item: fecsStatusBar});
     },
-    deactivate: function () {
+    deactivate() {
         disposables && disposables.dispose();
         mainDisposables && mainDisposables.dispose();
         editor = null;
     }
-
 };
